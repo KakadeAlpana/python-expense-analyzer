cfa5cc195880ca6f51f640a0f2e5012b1881f5c5
KakadeAlpana <alpanakakde@gmail.com>
2026-09-25 19:39:07 +0530

    main.py updated

--- expenses-analyzer_UI/backend/app/main.py
+++ expenses-analyzer_UI/backend/app/main.py
@@ -74,10 +74,11 @@ A Python FastAPI backend for the Expense Analyzer application.
 # -------------------------------------------------------------
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",   # Vite dev server (React frontend)
-        "http://127.0.0.1:5173",   # Alternative localhost format
-    ],
+   allow_origins=[
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://python-expense-analyzer-frontend.onrender.com",
+],
     allow_credentials=True,
     allow_methods=["*"],   # Allow all HTTP methods
     allow_headers=["*"],   # Allow all headers
